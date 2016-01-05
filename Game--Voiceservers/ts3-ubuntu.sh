19f79a6adb18d2b485e59d4fa533828ba496eb45
scriptaday <ympker-online@web.de>
2016-01-05 18:46:59 +0800

    Delete ts3-ubuntu.sh

--- Game--Voiceservers/ts3-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get install sudo
-sudo apt-get install tmux mailutils postfix curl  libmariadb2
-adduser ts3server
-passwd ts3server
-su - ts3server
-wget http://gameservermanagers.com/dl/ts3server
-chmod +x ts3server
-./ts3server install
